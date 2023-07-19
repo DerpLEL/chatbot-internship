6fb412a4c2663b7d404c4ed2e7fa8ec703f8ca5c
DerpLEL <honguyenngocbao@gmail.com>
2023-07-19 09:45:12 +0700

    Changed format instructions to new template

--- agent.py
+++ agent.py
@@ -310,6 +310,21 @@ def datetime_calc(query: str):
         return exec(query)
 
 
+format_instr = '''
+Structure your response with the following format:
+
+Question: ```the input question you must answer```
+Thought: ```you should always think about what to do to answer the input question```
+Action: ```the tool you select to use, which should be one of the following: {tool_names}```
+Action Input: ```the input to the tool you selected```
+Observation: ```the result of the action, this will be computed and given back to you based on your action input```
+
+Repeat the Thought/Action/Action Input/Observation cycle until you find an answer to the Question. Then, when you have the final answer to the question, use the following format:
+
+Thought: ```I now know the final answer```
+Final Answer: ```your final answer to the original input question```
+'''
+
 tool1 = [
     Tool(
         name='HRM get user data',
@@ -553,7 +568,7 @@ suffix2 = """Begin!
 
 {context}
 Question: {input}
-{agent_scratchpad} 
+Thought: {agent_scratchpad} 
 """
 
 suffix2 = temp2 + suffix2
@@ -561,6 +576,7 @@ suffix2 = temp2 + suffix2
 prompt2 = ZeroShotAgent.create_prompt(
     tool2,
     prefix=prefix2,
+    format_instructions=format_instr,
     suffix=suffix2,
     input_variables=["input", "context", "agent_scratchpad"],
 )
