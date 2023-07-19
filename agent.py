10b9263fa59623193a4ba7fc5439430f1f7d7430
DerpLEL <honguyenngocbao@gmail.com>
2023-07-19 10:46:19 +0700

    Changed the main model used to 4k version for bug checking

--- agent.py
+++ agent.py
@@ -35,6 +35,22 @@ llm3 = AzureChatOpenAI(
     max_tokens=600
 )
 
+format_instr = '''
+Structure your response with the following format:
+
+Question: ```the input question you must answer```
+Thought: ```you should always think about what to do to answer the input question```
+Action: ```the tool you select to use, which should be one of the following: {tool_names}```
+Action Input: ```the input to the tool you selected```
+Observation: ```the result of the action, this will be computed and given back to you based on your action input```
+
+Repeat the Thought/Action/Action Input/Observation cycle until you find an answer to the Question. 
+Then, when you have the final answer to the question, use the following format:
+
+Thought: ```I now know the final answer```
+Final Answer: ```your final answer to the original input question```
+'''
+
 
 class ZSAgentMod(ZeroShotAgent):
     @property
@@ -317,21 +333,6 @@ def datetime_calc(query: str):
     except Exception:
         return exec(query)
 
-
-format_instr = '''
-Structure your response with the following format:
-
-Question: ```the input question you must answer```
-Thought: ```you should always think about what to do to answer the input question```
-Action: ```the tool you select to use, which should be one of the following: {tool_names}```
-Action Input: ```the input to the tool you selected```
-Observation: ```the result of the action, this will be computed and given back to you based on your action input```
-
-Repeat the Thought/Action/Action Input/Observation cycle until you find an answer to the Question. Then, when you have the final answer to the question, use the following format:
-
-Thought: ```I now know the final answer```
-Final Answer: ```your final answer to the original input question```
-'''
 
 tool1 = [
     Tool(
