c44dd2531fd21ef12f1005920f2c732adbef23cf
DerpLEL <honguyenngocbao@gmail.com>
2023-07-31 15:24:07 +0700

    Added 2 columns, post_leave and del_leave

--- database_test.py
+++ database_test.py
@@ -9,6 +9,12 @@ conn = pyodbc.connect(f'DRIVER={driver};SERVER=tcp:{server};PORT=1433;DATABASE={
 
 cursor = conn.cursor()
 
+# Current database sql-chatbot contains 4 columns
+# email: user's email, as the primary key
+# chat: chat history between the user and the bot
+# post_leave: placeholder
+# del_leave: placeholder
+
 # cursor.execute("""CREATE TABLE history (
 #     email varchar(255) PRIMARY KEY,
 #     chat TEXT
@@ -27,15 +33,35 @@ cursor = conn.cursor()
 # for i in rows:
 #     print(i[0])
 
-def add_to_history(query, response, email, n=3):
+
+def add_to_history_sql(query, response, email):
+    n = 3
     cursor.execute(f"""SELECT chat FROM history WHERE email = '{email}';""")
     hist = cursor.fetchone()[0].split('<sep>')
 
     hist.append(f"{query}||{response}")
+    if len(hist) > n:
+        hist = hist[len(hist) - n:]
+
     res = "<sep>".join(hist)
+    cursor.execute(f"""UPDATE history
+SET chat = '{res}' WHERE email = '{email}';""")
+    conn.commit()
 
 
+def get_history_as_txt_sql(email):
+    cursor.execute(f"""SELECT chat FROM history WHERE email = '{email}';""")
+    hist = cursor.fetchone()[0].split('<sep>')
 
-    return res
+    txt = ""
+    for row in hist:
+        i = row.split('||')
 
-print(add_to_history('Hello', 'dingus', 'bao.ho@nois.vn'))
+        txt += f"\n<|im_start|>user\n{i[0]}\n<|im_end|>\n"
+        txt += f"<|im_start|>assistant\n{i[1]}\n<|im_end|>"
+
+    return txt
+
+
+# print(get_history_as_txt_sql('bao.ho@nois.vn'))
+cursor.execute("""SELECT * FROM history;""")
