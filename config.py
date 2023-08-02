d1de61d097b89118428b92f417c70f9714b7bdaf
DerpLEL <honguyenngocbao@gmail.com>
2023-08-02 15:27:05 +0700

    Made small changes to post-leave, added a temporary token for testing

--- config.py
+++ config.py
@@ -6,13 +6,18 @@ import os
 
 """ Bot Configuration """
 
+# Default
 id = "b8838874-d6d2-4747-a8c7-862d2f530db0"
 pw = "Gf~8Q~wMv-0j1TwBFlOy4mJauAE2eDKfwwXnTalx"
+
+# azure-intern01
+id01 = "d70aeb25-d473-4545-b414-70e9050580bf"
+pw01 = "GdD8Q~63pxG~gNl4IRkPXRWOfyxAbrhl2~TRKanF"
 
 class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", id)
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", pw)
+    APP_ID = os.environ.get("MicrosoftAppId", id01)
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", pw01)
     CONNECTION_NAME = os.environ.get("ConnectionName", "BotLogin")
