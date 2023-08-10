085275a26499c08251b67e3a439e3bbeb23dc3c2
DerpLEL <honguyenngocbao@gmail.com>
2023-08-10 09:20:44 +0700

    idk

--- dialogs/chatbot.py
+++ dialogs/chatbot.py
@@ -1,3 +1,4 @@
+import pyodbc
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
